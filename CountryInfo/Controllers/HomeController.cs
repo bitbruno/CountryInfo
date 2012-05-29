7f714bacf9bb04ccfbf89e7f4b907cbe9ceb31cb
bitbruno <bitbruno@gmail.com>
2012-05-29 16:11:15 -0300

    -Now when the user select a continent it is showed the list of countries of that continent.

--- CountryInfo/Controllers/HomeController.cs
+++ CountryInfo/Controllers/HomeController.cs
@@ -10,14 +10,34 @@ namespace CountryInfo.Controllers
     public class HomeController : Controller
     {
         private CountryInfoModelDataContext db = new CountryInfoModelDataContext();  
+        
+        
         public ActionResult Index()
         {
             //show the list of countinents in the index page.
-            
-
             ViewBag.Message = "List of Continents";
             return View(db.Continents.ToList());
         }
+
+
+        public ActionResult CountryInfo(int id = 0)
+        {
+            ViewBag.Message = "List of Countries";
+            ViewBag.continentID = id;
+
+            //select the countries that belong to the selected country
+            var countries = from c in db.Countries where c.Continent_ID == id select c;
+
+            if (id == 0 || countries.Count() == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            //show a list of continents
+            return View(countries);
+        }
+
+
 
         public ActionResult About()
         {
