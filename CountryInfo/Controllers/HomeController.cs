f68a874fb76dcb8245d179314e34a4d84492f68b
bitbruno <bitbruno@gmail.com>
2012-05-29 14:57:21 -0300

    -Added the LINQ to SQL class CountryInfoModel that creates the relationship with the database. -Modified the index page to show the list of continents.

--- CountryInfo/Controllers/HomeController.cs
+++ CountryInfo/Controllers/HomeController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CountryInfo.Models;
 
 namespace CountryInfo.Controllers
 {
     public class HomeController : Controller
     {
+        private CountryInfoModelDataContext db = new CountryInfoModelDataContext();  
         public ActionResult Index()
         {
             //show the list of countinents in the index page.
+            
 
-            return View();
+            ViewBag.Message = "List of Continents";
+            return View(db.Continents.ToList());
         }
 
         public ActionResult About()
